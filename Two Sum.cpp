a7402c45ae5755256720c1fee6be9c1292486020
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-04-22 16:04:25 +0530

    1

---
+++ Two Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a=0,b=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                int sum = 0;
+                sum = nums[i] + nums[j];
+                if(sum==target)
+                {
+                    a=i;
+                    b = j;
+                }
+                    
+            }
+        }
+        return {a,b};
+    }
+};
